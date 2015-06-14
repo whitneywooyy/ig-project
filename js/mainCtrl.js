e32a114157e3d776c781fa8499ffc061c6bceb56
Whitney <whitneywooyy@gmail.com>
2015-06-13 18:03:03 -0600

    Ready to go live

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -3,6 +3,7 @@ var app = angular.module('anywhereintheworld');
 app.controller('mainCtrl', function($scope, homeService){
 
 	$scope.searchTerm = function(){
+		
 		homeService.searchTerm($scope.location, $scope.near).then(function(resFromService){
 			// console.log("resFromService", resFromService);
 			$scope.searchResults = resFromService;
